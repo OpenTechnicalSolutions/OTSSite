55d685513d739763756a9a753770b87ee421e72c
Allen Roberts <allen.roberts1985@gmail.com>
2018-10-15 00:23:38 -0400

    Initial Commit

---
+++ OTSSite/OTSSite/Repositories/CommentRepository.cs
@@ -0,0 +1,64 @@
+﻿using OTSSite.Data;
+using OTSSite.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OTSSite.Repositories
+{
+    public class CommentRepository : IRepository<CommentModel>
+    {
+        private ApplicationDbContext _dbContext;
+
+        public CommentRepository(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public void Add(CommentModel data)
+        {
+            _dbContext.Comments.Add(data);
+        }
+
+        public void Delete(int id)
+        {
+            Delete(_dbContext.Comments.FirstOrDefault(c => c.Id == id));
+        }
+
+        public void Delete(CommentModel data)
+        {
+            Delete(data);
+        }
+
+        public IQueryable<CommentModel> GetAll()
+        {
+            return (_dbContext.Comments);
+        }
+
+        public CommentModel GetById(int id)
+        {
+            return (_dbContext.Comments.FirstOrDefault(c => c.Id == id));
+        }
+
+        public void Update(CommentModel data)
+        {
+            _dbContext.Comments.Update(data);
+        }
+
+        public IQueryable<CommentModel> GetArticleComments(int articleId)
+        {
+            return (_dbContext.Comments.Where(c => c.Id == articleId));
+        }
+
+        public IQueryable<CommentModel> GetTopLevelComments(int articleId)
+        {
+            return (GetArticleComments(articleId).Where(c => c.ReplyCommentId == -1));
+        }
+
+        public IQueryable<CommentModel> GetReplyComments(int replyCommentId)
+        {
+            return (_dbContext.Comments.Where(c => c.ReplyCommentId == replyCommentId));
+        }
+    }
+}
---
+++ OTSSite/OTSSite/Repositories/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OTSSite.Repositories
+{
+    public interface IRepository<T> where T : class
+    {
+        void Add(T data);
+        void Update(T data);
+        IQueryable<T> GetAll();
+        T GetById(int id);
+        void Delete(int id);
+        void Delete(T data);
+    }
+}
