64e47c32210e9fbf5e436e699920d24d5ae0727f
Allen Roberts <allen.roberts1985@gmail.com>
2018-10-17 15:34:05 -0400

    changed namespace name

--- OTSSite/OTSSite2/Startup.cs
+++ OTSSite/OTSSite2/Startup.cs
@@ -9,7 +9,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using OTSSite2.Data;
+using OTSSite.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
