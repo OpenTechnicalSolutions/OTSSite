166065d2cae50af2373bf2a5e2dd410bb6a49c4c
Allen Roberts <allen.roberts1985@gmail.com>
2018-10-26 00:46:54 -0400

    MVC

---
+++ OTSSite/OTSSite/Repositories/CommentRepository.cs
@@ -0,0 +1,66 @@
+﻿using OTSSite.Data;
+using OTSSite.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OTSSite.Repositories
+{
+    public class CommentRepository : IRepository<Comment>
+    {
+        public ApplicationDbContext _dbContext;
+
+        public CommentRepository(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public void Add(Comment item)
+        {
+            _dbContext.Comments.Add(item);
+            _dbContext.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            Delete(_dbContext.Comments.FirstOrDefault(c => c.Id == id));
+
+        }
+
+        public void Delete(Comment item)
+        {
+            _dbContext.Comments.Remove(item);
+            _dbContext.SaveChanges();
+        }
+
+        public IEnumerable<Comment> GetAll()
+        {
+            return _dbContext.Comments;
+        }
+
+        public Comment GetById(int id)
+        {
+            return GetAll().FirstOrDefault(c => c.Id == id);
+        }
+
+        public void Update(Comment item)
+        {
+            _dbContext.Comments.Update(item);
+            _dbContext.SaveChanges();
+        }
+
+        public IEnumerable<Comment> GetByArticleId(int articleId)
+        {
+            return GetAll().Where(c => c.ArticleId == articleId);
+        }
+
+        public IEnumerable<Comment> GetTopLevel(int articleId)
+        {
+            return GetByArticleId(articleId).Where(c => c.ReplyId == 0);
+        }
+
+        public IEnumerable<Comment> GetChildComments(int replyId)
+        {
+            return GetAll().Where(c => c.ReplyId == replyId);
+        }
+    }
+}
