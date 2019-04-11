33d5d1648d861eb5d0a0275410f9b9a53164b0f7
Allen Roberts <allen.roberts1985@gmail.com>
2019-04-10 23:39:16 -0400

    Added submit article

---
+++ OTSSite/OTSSite/Pages/Publish/SubmitArticle.cshtml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using OTSSite.Entities;
+using OTSSite.Repositories;
+
+namespace OTSSite.Pages.Publish
+{
+    public class SubmitArticleModel : PageModel
+    {
+        private readonly IRepository<Article> _articleRepository;
+        private readonly IRepository<Comment> _commentArticle;
+        private readonly UserManager<ApplicationIdentityUser> _userManager;
+
+        public SubmitArticleModel(
+            IRepository<Article> articleRepository,
+            IRepository<Comment> commentRepository,
+            UserManager<ApplicationIdentityUser> userManager)
+        {
+            _articleRepository = articleRepository;
+            _commentArticle = commentRepository;
+            _userManager = userManager;
+        }
+        public void OnGet()
+        {
+
+        }
+    }
+}
