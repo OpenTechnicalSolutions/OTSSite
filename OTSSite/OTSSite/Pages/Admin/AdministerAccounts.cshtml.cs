d3f90fb9c0db76f2d24ae1480bf5c250dbf22a26
Allen Roberts <allen.roberts1985@gmail.com>
2019-04-22 19:44:08 -0400

    Started Admin pages

---
+++ OTSSite/OTSSite/Pages/Admin/AdministerAccounts.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using OTSSite.Entities;
+
+namespace OTSSite.Pages.Admin
+{
+    public class AdministerAccountsModel : PageModel
+    {
+        private readonly UserManager<ApplicationIdentityUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public AdministerAccountsModel(
+            UserManager<ApplicationIdentityUser> userManager,
+            RoleManager<IdentityRole> roleManager)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
