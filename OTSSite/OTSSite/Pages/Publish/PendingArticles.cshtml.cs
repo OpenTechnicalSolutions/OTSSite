badad7696c378ff68a82a0a7adb937c0d587ca25
Allen Roberts <allen.roberts1985@gmail.com>
2019-04-22 17:44:47 -0400

    added each type of status page.

---
+++ OTSSite/OTSSite/Pages/Publish/PendingArticles.cshtml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using OTSSite.Entities;
+using OTSSite.Models.ViewModels;
+using OTSSite.Repositories;
+
+namespace OTSSite.Pages.Publish
+{
+    public class PendingArticlesModel : PageModel
+    {
+        private readonly IRepository<Article> _articleRepository;
+        private readonly UserManager<ApplicationIdentityUser> _userManager;
+
+        public List<ArticleStatusViewModel> PendingArticleViewModels { get; set; }
+
+        public PendingArticlesModel(
+            IRepository<Article> articleRepository, 
+            UserManager<ApplicationIdentityUser> userManager)
+        {
+            _articleRepository = articleRepository;
+            _userManager = userManager;
+        }
+        public IActionResult OnGet()
+        {
+            var articlesFromEntity = _articleRepository.GetAllPending();
+            PendingArticleViewModels = new List<ArticleStatusViewModel>();
+            foreach(var a in articlesFromEntity)
+            {
+                var articlePendingVM = Mapper.Map<ArticleStatusViewModel>(a);
+                articlePendingVM.AuthorUserName = _userManager.Users.FirstOrDefault(u => a.AuthorId == u.Id).UserName;
+                PendingArticleViewModels.Add(articlePendingVM);
+            }
+            return Page();
+        }
+    }
+}
