f998f6831aa5429273840532f5fc20c4898274f1
Allen Roberts <allen.roberts1985@gmail.com>
2019-05-07 21:38:42 -0400

    started on working with an API

---
+++ OTSSite/OTSSiteMVC/Controllers/AccountApiController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using OTSSiteMVC.Entities;
+
+namespace OTSSiteMVC.Controllers
+{
+    [Route("api/Accounts")]
+    [ApiController]
+    public class AccountApiController : ControllerBase
+    {
+        private readonly UserManager<AppIdentityUser> _userManager;
+
+        public AccountApiController(
+            UserManager<AppIdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
+    }
+}
